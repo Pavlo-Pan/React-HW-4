7fe8f10c8b3d28eafc99ac7a7bff3d5854864340
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-03-06 15:31:55 +0100

    add hw4

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import CityCard from "../CityCard/CityCard"
+import MathQuiz from "../MathQuiz/MathQuiz"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <CityCard />
+      <MathQuiz/>
+    </>
+  )
+}
+
+export default App
